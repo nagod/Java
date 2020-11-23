86470d4be5d733953a5db5211dc87a9608b90eb2
nagod <deniz-dogan@t-online.de>
2020-11-23 23:51:59 +0100

    added algo folder

---
+++ Algos/NestedScopesExample.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class NestedScopesExample {
+    String title = "Deniz";
+
+    public void printTitle(){
+        System.out.println(title);
+        String title="Dogan";
+        System.out.println(title);
+    }
+
+    public static void main(String [] args){
+
+        NestedScopesExample tmp = new NestedScopesExample();
+        tmp.printTitle();
+    }
+}
